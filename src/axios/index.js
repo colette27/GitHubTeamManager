65d6b93487b37e23da29af4bd5e5e6a48a8334bd
bohemeprincess <colette@starter.io>
2020-04-20 09:52:48 -0700

    Update index.js

--- src/axios/index.js
+++ src/axios/index.js
@@ -3,7 +3,7 @@ import axios from "axios";
 let PERSONAL_ACCESS_TOKEN = "49b2e1c194745fe878bfe5a4e2c28227da3eaa39";
 
 export default axios.create({
- // baseURL: "https://api.github.com/organizations/62528634/",
+ 
    baseURL: "https://api.github.com/orgs/github-JavaSpring/",
   headers: {
     Authorization: "token " + PERSONAL_ACCESS_TOKEN,
